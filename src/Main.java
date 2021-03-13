e23fb6025bed919809263a719a29d6dc12ebe65a
Veronika <n.kanchura.dodo@gmail.com>
2021-03-13 20:21:42 +0400

    (jv)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        long amount = 3500;
+        long price = 20;
+
+        long mile = amount / price;
+
+        System.out.println(mile);
+
+    }
+}
+
